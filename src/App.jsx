1fdfdd04018bb358f07198db0d5770af52e48923
jtr-code <jtrtricks@gmail.com>
2024-05-27 05:10:51 +0530

    🎉 init: tailwind initialize

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold">Hello, Tailwind CSS!</h1>
+    </div>
+  );
 };
 
 export default App;
