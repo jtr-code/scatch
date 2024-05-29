6312baa263bcc53a3960fccb2db45fba2a1797d9
jishnumoshi <jishnutraj.moshimoshi@gmail.com>
2024-05-29 10:15:27 +0530

    🐞 fix: add responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,9 @@ import Register from "../../components/Register";
 
 const Home = () => {
   return (
-    <main className="flex justify-around flex-wrap items-center px-16">
+    <main className="flex justify-around flex-wrap gap-8 items-center lg:px-16">
       <Register />
-      <article className="w-10 h-10 bg-blue-500 text-white rounded-full flex justify-center items-center">
+      <article className="w-10 h-10 bg-blue-500 text-white rounded-full flex flex-wrap justify-center items-center ">
         <p>or</p>
       </article>
       <Login />
